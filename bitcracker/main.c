c0d1f8c9866d04124d007ee501e340b9bf5ab321
Jason <init6.init6.me>
2013-10-10 23:07:12 -0500

    starting to work on bitcracker
    I don't know see very well but trying to learn.

---
+++ bitcracker/main.c
@@ -0,0 +1,29 @@
+
+FILE *ifp;
+char *mode = "r";
+
+ifp = fopen("bitcrack.conf", mode);
+
+if (ifp == NULL) {
+  fprintf(stderr, "Can't open bitrack.conf\n");
+  exit(1);
+}
+
+while(fgets(
+
+#file layout
+void
+void
+inBTC_address
+inPubKey
+inCkey
+void
+void
+void
+inCryptedKey
+inSalt
+inRounds
+inMethod
+#file layout
+
+fclose(ifp)
